f1ac8b1a9642d96351bc1bb161aa4e6e891f01fa
Fernando Dobladez <fernando@code54.com>
2012-03-15 12:32:13 -0300

    Add lein project to use for development

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject buildversion-maven-plugin "1.0.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.cloudhoist/clojure-maven-mojo-annotations "0.3.2-SNAPSHOT"]
+                 [org.cloudhoist/clojure-maven-mojo "0.3.2-SNAPSHOT"]
+                 [org.apache.maven/maven-plugin-api "3.0.3"]
+                 [org.clojure/tools.trace "0.7.1"]]
+
+  :source-path "src/main/clojure"
+  :compile-path "target/classes"
+  :library-path "target/dependency"
+  :test-path  "src/test/clojure"
+  :resources-path "src/main/resource"
+  :target-dir "target"
+  )
