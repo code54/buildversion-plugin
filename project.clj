dc815bccd6fcc8c4195fc9e33f339d9f63fc9225
Fernando Dobladez <fernando@code54.com>
2012-03-31 18:31:51 -0300

    Add some dev-dependencies when using lein

--- project.clj
+++ project.clj
@@ -5,6 +5,9 @@
                  [org.apache.maven/maven-plugin-api "3.0.3"]
                  [org.clojure/tools.trace "0.7.1"]
                  [conch "0.2.1"]]
+  :dev-dependencies [[lein-swank "1.4.4"]
+                     [lein-pprint "1.1.1"]
+                     [radagast "1.1.0"]]
 
   :source-path "src/main/clojure"
   :compile-path "target/classes"
