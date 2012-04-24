461eae64143cadc2b1dc8ebb664048b89cb07963
Fernando Dobladez <fernando@code54.com>
2012-04-24 16:36:38 -0300

    Prepare release 1.0.0

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject buildversion-maven-plugin "1.0.0-SNAPSHOT"
+(defproject buildversion-maven-plugin "1.0.0"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.cloudhoist/clojure-maven-mojo-annotations "0.3.2-SNAPSHOT"]
-                 [org.cloudhoist/clojure-maven-mojo "0.3.2-SNAPSHOT"]
+                 ;[org.cloudhoist/clojure-maven-mojo-annotations "0.3.2-SNAPSHOT"]
+                 [org.cloudhoist/clojure-maven-mojo-annotations "0.3.1"]
+                 ;[org.cloudhoist/clojure-maven-mojo "0.3.2-SNAPSHOT"]
+                 [org.cloudhoist/clojure-maven-mojo "0.3.1"]
                  [org.apache.maven/maven-plugin-api "3.0.3"]
                  [org.clojure/tools.trace "0.7.1"]
                  [conch "0.2.1"]]
