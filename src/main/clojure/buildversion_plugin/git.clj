4970a1df8f80fe64212a7d10bd07ba0899e0428b
Fernando Dobladez <fernando@code54.com>
2012-08-23 16:37:21 -0300

    Minor code re-formatting

--- src/main/clojure/buildversion_plugin/git.clj
+++ src/main/clojure/buildversion_plugin/git.clj
@@ -31,8 +31,8 @@
                  (str "Execution of Git command failed: " stderr)))))))
 
 (defn git-describe-log-lines [log-lines-seq]
-  "Given a seq of \"git log\" output lines, return map with :git-tag (most recent tag)
-   and :git-tag-delta (number of commits to reach it)"
+  "Given a seq of \"git log\" output lines, return map with :git-tag (most recent
+  tag) and :git-tag-delta (number of commits to reach it)"
   (loop [i 0, lines log-lines-seq]
     (let [line (first lines)
           [_ hash tag] (re-find #"^(\w+) .*tag: (v[^\)\,]+).*" line)]
@@ -42,8 +42,9 @@
         [tag i]))))
 
 (defn git-describe-first-parent [dir]
-  "Return map with :git-tag (most recent tag on current branch (always following \"first-parent\" on merges))
-and :git-tag-delta (number of commits -couting on first-parent paths only- from :git-tag to HEAD) "
+  "Return map with :git-tag (most recent tag on current branch (always following
+  \"first-parent\" on merges)) and :git-tag-delta (number of commits -couting on
+  first-parent paths only- from :git-tag to HEAD)"
 
   (let [p (run-git dir ["log" "--oneline" "--decorate=short" "--first-parent"])
         lines (line-seq (reader (:out p)))
@@ -58,33 +59,35 @@ and :git-tag-delta (number of commits -couting on first-parent paths only- from 
   "Infer the current project version from tags on the source-control system"
 
   (binding [*debug-fn* debug-fn, *git-cmd* git-cmd]
-      (let [commit-tstamp (-> (run-git-wait dir ["log" "-n" "1" "--format=%ct"])
-                              trim-newline
-                              (Long/parseLong)
-                              (* 1000)
-                              Date.)
-            format-str (or tstamp-format "yyyyMMddHHmmss")
-            commit-tstamp-str (.format (SimpleDateFormat. format-str)
-                                       commit-tstamp)
+    (let [commit-tstamp (-> (run-git-wait dir ["log" "-n" "1" "--format=%ct"])
+                            trim-newline
+                            (Long/parseLong)
+                            (* 1000)
+                            Date.)
+          format-str (or tstamp-format "yyyyMMddHHmmss")
+          commit-tstamp-str (.format (SimpleDateFormat. format-str)
+                                     commit-tstamp)
 
-            [short-hash long-hash] (split
-                                    (run-git-wait dir [ "log" "-n" "1" "--format=%h %H" ])
-                                    #" " )
+          [short-hash long-hash]  (split
+                                   (run-git-wait dir
+                                                 ["log" "-n" "1" "--format=%h %H"])
+                                   #" ")
 
-            versioning-properties {:build-tag "N/A"
-                                   :build-version "N/A"
-                                   :build-tag-delta "0"
-                                   :build-tstamp commit-tstamp-str
-                                   :build-commit long-hash
-                                   :build-commit-abbrev short-hash }
-            
-            {:keys [git-tag git-tag-delta] } (git-describe-first-parent dir)]
-
-        (if (nil? git-tag)
-          versioning-properties
+          versioning-properties {:build-tag "N/A"
+                                 :build-version "N/A"
+                                 :build-tag-delta "0"
+                                 :build-tstamp commit-tstamp-str
+                                 :build-commit long-hash
+                                 :build-commit-abbrev short-hash }
           
-          (let [maven-artifact-version ((re-find #"v(.*)" git-tag) 1)]
-            (merge versioning-properties
-                   {:build-tag maven-artifact-version
-                    :build-version (str (replace-first git-tag #"^v" "") "-" git-tag-delta "-" short-hash)
-                    :build-tag-delta (str git-tag-delta) }))))))
+          {:keys [git-tag git-tag-delta] } (git-describe-first-parent dir)]
+
+      (if (nil? git-tag)
+        versioning-properties
+        
+        (let [maven-artifact-version ((re-find #"v(.*)" git-tag) 1)]
+          (merge versioning-properties
+                 {:build-tag maven-artifact-version
+                  :build-version (str (replace-first git-tag #"^v" "") "-"
+                                      git-tag-delta "-" short-hash)
+                  :build-tag-delta (str git-tag-delta) }))))))
