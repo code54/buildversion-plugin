b8aea76f6276f46dc7916a174f66af97f4b54d74
Fernando Dobladez <fernando@code54.com>
2012-10-22 23:22:47 -0300

    Minor change to finish-release script

--- finish-release.sh
+++ finish-release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# finish the release after updating release notes
+# Finish the release
 
 if [[ $# -lt 1 ]]; then
   echo "usage: $(basename $0) new-version" >&2
@@ -11,19 +11,14 @@ version=$1
 
 echo "finish release of $version"
 
-echo -n "commiting release notes and readme.  enter to continue:" && read x \
-&& git add ReleaseNotes.md README.md \
-&& git commit -m "Updated release notes and readme for $version" \
-&& echo -n "Peform release.  enter to continue:" && read x \
+echo -n "Peform release.  enter to continue:" && read x \
 && mvn release:clean \
 && mvn release:prepare -Dgpg.keyname=02FCB552 \
-&& git checkout develop; git merge "release-${new-version}" \
-&& git checkout master; git merge "v${new-version}" \
-&& mvn release:perform -Dgpg.keyname=02FCB552
-
-# \
-#&& mvn nexus:staging-close \
-#&& mvn nexus:staging-promote \
+&& git checkout develop; git merge "release-${version}" \
+&& git checkout master; git merge "v${version}" \
+&& mvn release:perform -Dgpg.keyname=02FCB552 \
+&& mvn nexus:staging-close \
+&& mvn nexus:staging-promote
 
 
 #&& git flow release finish -n $version
