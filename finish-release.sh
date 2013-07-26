5103f592e56d82b2e5326551a2dcb85a9c2aea73
Fernando Dobladez <fernando@code54.com>
2013-07-26 11:47:57 -0300

    use staging-release

--- finish-release.sh
+++ finish-release.sh
@@ -16,7 +16,7 @@ echo -n "Perform release.  enter to continue:" && read x \
 && mvn release:prepare -Dgpg.keyname=02FCB552 \
 && mvn release:perform -Dgpg.keyname=02FCB552 \
 && mvn nexus:staging-close \
-&& mvn nexus:staging-promote \
+&& mvn nexus:staging-release \
 && git checkout develop; git merge "release-${version}" \
 && git checkout master; git merge "v${version}" \
 
