6fc541388dcc242592e0793cb31b77f5f1681d78
Ben Podgursky <bpodgursky@liveramp.com>
2015-08-24 19:19:17 -0700

    no need for bloba

--- src/test/bash/create-sample-git-project.sh
+++ src/test/bash/create-sample-git-project.sh
@@ -3,10 +3,10 @@
 echo "Running $0 ..."
 TARGET_DIR="$1"
 
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global alias.ci commit
-git config --global alias.st status
+git config alias.co checkout
+git config alias.br branch
+git config alias.ci commit
+git config alias.st status
 
 #rm -rf $TARGET_DIR
 mkdir -p $TARGET_DIR
