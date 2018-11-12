7cecacc3ec962e2f51d45f79300870d8652899a0
Carlos Latugaye <carlos@code54.com>
2018-11-12 17:18:50 -0300

    Fix project paths

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject buildversion-maven-plugin/buildversion-maven-plugin "1.0.1-SNAPSHOT" 
-  :compile-path "/home/ferd/repos/buildversion-plugin/target/classes"
   :dependencies [[org.clojure/clojure "1.3.0"]
+  :compile-path "target/classes"
                  [org.cloudhoist/clojure-maven-mojo-annotations
                   "0.3.2"]
                  [org.cloudhoist/clojure-maven-mojo "0.3.2"]
@@ -8,12 +8,12 @@
                  [org.clojure/tools.trace "0.7.1"]
                  [conch "0.2.1"]]
   :source-paths ["src/main/clojure"]
-  :jar-dir "/home/ferd/repos/buildversion-plugin/target"
+  :jar-dir "target"
   :profiles {:dev {:dependencies [[radagast "1.1.0"]]}}
   :repositories {"sonatype-releases"
                  "http://oss.sonatype.org/content/repositories/releases"}
   :resource-paths ["src/main/resource"]
-  :target-dir "/home/ferd/repos/buildversion-plugin/target"
+  :target-dir "target"
   :min-lein-version "2.0.0"
   :plugins [[lein-swank "1.4.4"] [lein-pprint "1.1.1"]]
   :test-paths ["src/test/clojure"]
