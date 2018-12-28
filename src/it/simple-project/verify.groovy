bae6ea954ee3b955d208a6cc220bc5878658af10
Carlos Latugaye <carlos@code54.com>
2018-12-28 11:47:36 -0300

    Fix plugin version on sample projects

--- src/it/simple-project/verify.groovy
+++ src/it/simple-project/verify.groovy
@@ -1,7 +1,7 @@
 
 def targetString = "build-version: 1.2.0-SNAPSHOT"
 
-// println "*** Checking build log output. Looking for build-version${targetString}"
+println "*** Checking build log output. Looking for build-version${targetString}"
 
 def build_log = new File("target/it/simple-project/build.log").text
 
